33e4f22b85994aebbdb9a61490e9c546052f8f21
NMunro <neilmunro@gmail.com>
2023-06-10 23:31:12 +0100

    Tidied up code formatting

--- src/app.lisp
+++ src/app.lisp
@@ -21,27 +21,25 @@
 (malaga/web/routes:defroute +app+ "/players/:player" #'malaga/views:player)
 (malaga/web/routes:defroute +app+ "/players/:player/cards" #'malaga/views:player-cards)
 
+;; warning: hardcoded "hunchentoot".
 (defun find-hunchentoot-thread (th) (search "hunchentoot" (bt:thread-name th)))
 
 (defun main (&key (server :hunchentoot) (address (or (uiop:getenv "MALAGA_ADDRESS") (machine-instance))) (port (parse-integer (uiop:getenv "MALAGA_PORT"))))
   (let ((app (start-app :server server :address address :port port)))
     ;; let the webserver run.
-    ;; warning: hardcoded "hunchentoot".
     (handler-case (bt:join-thread (find-if #'find-hunchentoot-thread (bt:all-threads)))
-    ;; Catch a user's C-c
-    (#+sbcl sb-sys:interactive-interrupt
-      () (progn
-           (format *error-output* "Aborting.~&")
-           (clack:stop app)
-           (uiop:quit)))
+        ;; Catch a user's C-c
+        (#+sbcl sb-sys:interactive-interrupt
+          ()
+          (progn
+            (format *error-output* "Aborting.~&")
+            (clack:stop app)
+            (uiop:quit)))
         (error (c) (format t "Woops, an unknown error occured:~&~a~&" c)))))
 
 (defun start-app (&key (server :hunchentoot) (address (or (uiop:getenv "MALAGA_ADDRESS") (machine-instance))) (port (parse-integer (uiop:getenv "MALAGA_PORT"))))
   (djula:add-template-directory (asdf:system-relative-pathname "malaga" "src/templates/"))
-  (clack:clackup +app+
-                 :server server
-                 :address address
-                 :port port))
+  (clack:clackup +app+ :server server :address address :port port))
 
 (defun stop-app (instance)
   (clack:stop instance))
